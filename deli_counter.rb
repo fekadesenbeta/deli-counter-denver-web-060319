a5547184111c31d365e69ecdc1227350f2c3d42a
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-24 21:26:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ def take_a_number(katz_deli, new_name)
   puts "Welcome, #{new_name}. You are number #{katz_deli.length} in line."
 end
 
-def number_serving(katz_deli)
+def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
